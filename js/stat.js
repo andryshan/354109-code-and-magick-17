d072eb28ea7d365b6b6dcc69fff66db9f62916c1
andryshan <avolkov123@mail.ru>
2019-06-06 01:51:14 +0300

    Перевел barHeight в константу, убрал комментарии, опасити заменил на насышенность, переименовал для этого функции и массив относящиеся к насышенности, убрал цикл из функции makeRandomSaturate

--- js/stat.js
+++ js/stat.js
@@ -14,27 +14,21 @@ var BAR_GAP_X = 50;
 var BAR_HEIGHT_MAX = 150;
 var NAME_HEIGHT = 30;
 var TIME_HEIGHT = 20;
-var barHeiht = CLOUD_HEIGHT - NAME_HEIGHT - BAR_GAP_Y;
-var EDGE_GAP = 40; // Отступ слева
+var BAR_HEIGHT = CLOUD_HEIGHT - NAME_HEIGHT - BAR_GAP_Y;
+var EDGE_GAP = 40;
 
-var RANDOM_NUMBERS_OPACITY = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
+var RANDOM_NUMBERS_SATURATE = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-/* var renderFont = function (ctx, color, font, baseline) {
-  ctx.fillStyle = color;
-  ctx.font = font;
-  ctx.textBaseline = baseline;
-}; */
-
-var getMaxElement = function (arr) {
-  var maxElement = arr[0];
-  for (var i = 1; i < arr.length; i++) {
-    if (arr[i] > arr[0]) {
-      maxElement = arr[i];
+var getMaxElement = function (array) {
+  var maxElement = array[0];
+  for (var i = 1; i < array.length; i++) {
+    if (array[i] > array[0]) {
+      maxElement = array[i];
     }
   }
   return maxElement;
@@ -48,18 +42,13 @@ var renderTextStats = function (ctx, text, x, y) {
   ctx.fillText(text, x, y);
 };
 
-var makeRandomOpacity = function (array) {
-  for (var i = 0; i < array.length; i++) {
-    return array[Math.floor(Math.random() * array.length)];
-  }
-  // Тут ошибку eslint убрал добавлением return
+var makeRandomSaturate = function (array) {
   return array[Math.floor(Math.random() * array.length)];
 };
 
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
-  // renderFont(ctx, '#000000', '16px PT Mono', 'hanging');
   ctx.fillStyle = '#000000';
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
@@ -72,43 +61,11 @@ window.renderStatistics = function (ctx, names, times) {
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'red';
     } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, ' + makeRandomOpacity(RANDOM_NUMBERS_OPACITY) + ')';
+      ctx.fillStyle = 'hsla(240, 100%, 50%, ' + makeRandomSaturate(RANDOM_NUMBERS_SATURATE) + ')';
     }
-    // Тут мне тоже кажется, от того, что создал функции renderBars и renderTextStats, легче от этого не стало.
-    renderBars(ctx, CLOUD_X + EDGE_GAP + ((BAR_WIDTH + BAR_GAP_X) * i), CLOUD_Y + BAR_GAP_Y + barHeiht - ((barHeiht * parseInt(times[i], 10)) / maxTime), BAR_WIDTH, (barHeiht * parseInt(times[i], 10)) / maxTime);
+    renderBars(ctx, CLOUD_X + EDGE_GAP + ((BAR_WIDTH + BAR_GAP_X) * i), CLOUD_Y + BAR_GAP_Y + BAR_HEIGHT - ((BAR_HEIGHT * parseInt(times[i], 10)) / maxTime), BAR_WIDTH, (BAR_HEIGHT * parseInt(times[i], 10)) / maxTime);
     ctx.fillStyle = '#000000';
     renderTextStats(ctx, names[i], CLOUD_X + EDGE_GAP + ((BAR_WIDTH + BAR_GAP_X) * i), CLOUD_Y + BAR_GAP_Y + BAR_HEIGHT_MAX + CLOUD_GAP);
-    renderTextStats(ctx, parseInt(times[i], 10), CLOUD_X + EDGE_GAP + ((BAR_WIDTH + BAR_GAP_X) * i), CLOUD_Y + BAR_GAP_Y + barHeiht - ((barHeiht * parseInt(times[i], 10)) / maxTime) - TIME_HEIGHT);
+    renderTextStats(ctx, parseInt(times[i], 10), CLOUD_X + EDGE_GAP + ((BAR_WIDTH + BAR_GAP_X) * i), CLOUD_Y + BAR_GAP_Y + BAR_HEIGHT - ((BAR_HEIGHT * parseInt(times[i], 10)) / maxTime) - TIME_HEIGHT);
   }
 };
-
-
-/* Была еще идея вынести в отдельную функцию (renderBarsStatsColors), которая рендерит бары и их цвет, цифры и имена, но что-то кажется что код от этого стал еще нечитабельней.
-var renderBarsStatsColors = function (ctx, names, times, maxTime) {
-  for (var i = 0; i < names.length; i++) {
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'red';
-    } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, ' + makeRandomOpacity(RANDOM_NUMBERS_OPACITY) + ')';
-    }
-    renderBars(ctx, CLOUD_X + EDGE_GAP + ((BAR_WIDTH + BAR_GAP_X) * i), CLOUD_Y + BAR_GAP_Y + barHeiht - ((barHeiht * parseInt(times[i], 10)) / maxTime), BAR_WIDTH, (barHeiht * parseInt(times[i], 10)) / maxTime);
-    ctx.fillStyle = '#000000';
-    renderTextStats(ctx, names[i], CLOUD_X + EDGE_GAP + ((BAR_WIDTH + BAR_GAP_X) * i), CLOUD_Y + BAR_GAP_Y + BAR_HEIGHT_MAX + CLOUD_GAP);
-    renderTextStats(ctx, parseInt(times[i], 10), CLOUD_X + EDGE_GAP + ((BAR_WIDTH + BAR_GAP_X) * i), CLOUD_Y + BAR_GAP_Y + barHeiht - ((barHeiht * parseInt(times[i], 10)) / maxTime) - TIME_HEIGHT);
-  }
-};
-
-window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
-  ctx.fillStyle = '#000000';
-  ctx.font = '16px PT Mono';
-  ctx.textBaseline = 'hanging';
-
-  ctx.fillText('Ура вы победили!', PARAGRAPH_X, PARAGRAPH_Y);
-  ctx.fillText('Список результатов:', PARAGRAPH_X, PARAGRAPH_Y + PARAGRAPH_GAP_Y);
-
-  var maxTime = getMaxElement(times);
-  renderBarsStatsColors(ctx, names, times, maxTime);
-};
-*/
