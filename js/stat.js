4d29b1671a4d23e98310784889bc1be898551d88
andryshan <avolkov123@mail.ru>
2019-06-06 20:14:19 +0300

    Функция renderSquares для рендера столбцов и облаков, перемнныe leftIndent и proportionBar для удобства

--- js/stat.js
+++ js/stat.js
@@ -10,19 +10,13 @@ var PARAGRAPH_GAP_Y = 20;
 var BAR_WIDTH = 40;
 var BAR_GAP_Y = 90;
 var BAR_GAP_X = 50;
-
 var BAR_HEIGHT_MAX = 150;
 var NAME_HEIGHT = 30;
 var TIME_HEIGHT = 20;
-var BAR_HEIGHT = CLOUD_HEIGHT - NAME_HEIGHT - BAR_GAP_Y;
 var EDGE_GAP = 40;
+var BAR_HEIGHT = CLOUD_HEIGHT - NAME_HEIGHT - BAR_GAP_Y;
 
 var RANDOM_NUMBERS_SATURATE = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
-
-var renderCloud = function (ctx, x, y, width, height, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, width, height);
-};
 
 var getMaxElement = function (array) {
   var maxElement = array[0];
@@ -34,12 +28,6 @@ var getMaxElement = function (array) {
   return maxElement;
 };
 
-/*
-var renderBars = function (ctx, width, height, x, y) {
-  ctx.fillRect(width, height, x, y);
-};
-*/
-
 var renderTextStats = function (ctx, text, x, y) {
   ctx.fillText(text, x, y);
 };
@@ -48,15 +36,19 @@ var makeRandomSaturate = function (array) {
   return array[Math.floor(Math.random() * array.length)];
 };
 
+var renderSquares = function (ctx, x, y, width, height, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height);
+};
+
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, CLOUD_WIDTH, CLOUD_HEIGHT, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, '#ffffff');
+  renderSquares(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, CLOUD_WIDTH, CLOUD_HEIGHT, 'rgba(0, 0, 0, 0.7)');
+  renderSquares(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, '#ffffff');
   ctx.fillStyle = '#000000';
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
-
-  ctx.fillText('Ура вы победили!', PARAGRAPH_X, PARAGRAPH_Y);
-  ctx.fillText('Список результатов:', PARAGRAPH_X, PARAGRAPH_Y + PARAGRAPH_GAP_Y);
+  renderTextStats(ctx, 'Ура вы победили!', PARAGRAPH_X, PARAGRAPH_Y);
+  renderTextStats(ctx, 'Список результатов:', PARAGRAPH_X, PARAGRAPH_Y + PARAGRAPH_GAP_Y);
 
   var maxTime = getMaxElement(times);
   for (var i = 0; i < names.length; i++) {
@@ -65,16 +57,11 @@ window.renderStatistics = function (ctx, names, times) {
     } else {
       ctx.fillStyle = 'hsla(240, 100%, 50%, ' + makeRandomSaturate(RANDOM_NUMBERS_SATURATE) + ')';
     }
-    renderCloud(ctx, CLOUD_X + EDGE_GAP + ((BAR_WIDTH + BAR_GAP_X) * i), CLOUD_Y + BAR_GAP_Y + BAR_HEIGHT - ((BAR_HEIGHT * parseInt(times[i], 10)) / maxTime), BAR_WIDTH, (BAR_HEIGHT * parseInt(times[i], 10)) / maxTime);
-    ctx.fillStyle = '#000000';
-    renderTextStats(ctx, names[i], CLOUD_X + EDGE_GAP + ((BAR_WIDTH + BAR_GAP_X) * i), CLOUD_Y + BAR_GAP_Y + BAR_HEIGHT_MAX + CLOUD_GAP);
-    renderTextStats(ctx, parseInt(times[i], 10), CLOUD_X + EDGE_GAP + ((BAR_WIDTH + BAR_GAP_X) * i), CLOUD_Y + BAR_GAP_Y + BAR_HEIGHT - ((BAR_HEIGHT * parseInt(times[i], 10)) / maxTime) - TIME_HEIGHT);
-    /*
     var proportionBar = BAR_HEIGHT * parseInt(times[i], 10) / maxTime;
-    renderBars(ctx, CLOUD_X + EDGE_GAP + ((BAR_WIDTH + BAR_GAP_X) * i), CLOUD_Y + BAR_GAP_Y + BAR_HEIGHT - proportionBar, BAR_WIDTH, proportionBar);
+    var leftIndent = CLOUD_X + EDGE_GAP + ((BAR_WIDTH + BAR_GAP_X) * i);
+    renderSquares(ctx, leftIndent, CLOUD_Y + BAR_GAP_Y + BAR_HEIGHT - proportionBar, BAR_WIDTH, proportionBar);
     ctx.fillStyle = '#000000';
-    renderTextStats(ctx, names[i], CLOUD_X + EDGE_GAP + ((BAR_WIDTH + BAR_GAP_X) * i), CLOUD_Y + BAR_GAP_Y + BAR_HEIGHT_MAX + CLOUD_GAP);
-    renderTextStats(ctx, parseInt(times[i], 10), CLOUD_X + EDGE_GAP + ((BAR_WIDTH + BAR_GAP_X) * i), CLOUD_Y + BAR_GAP_Y + BAR_HEIGHT - proportionBar - TIME_HEIGHT);
-    */
+    renderTextStats(ctx, names[i], leftIndent, CLOUD_Y + BAR_GAP_Y + BAR_HEIGHT_MAX + CLOUD_GAP);
+    renderTextStats(ctx, parseInt(times[i], 10), leftIndent, CLOUD_Y + BAR_GAP_Y + BAR_HEIGHT - proportionBar - TIME_HEIGHT);
   }
 };
