854a8667f60c2fce541c905e7ffbc7367e0292d0
andryshan <avolkov123@mail.ru>
2019-06-24 14:27:47 +0300

    Повторяющийся код в backend.js вынес в отдельную функцию

--- js/backend.js
+++ js/backend.js
@@ -1,26 +1,32 @@
 'use strict';
 (function () {
+  var xhrSetup = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json'; // Распаршиваем в JSON (Нормальный js обьект)
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response); // ответ
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    return xhr;
+  };
+
   window.backend = {
     save: function (data, onLoad, onError) {
       var URL = 'https://js.dump.academy/code-and-magick';
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json'; // Распаршиваем в JSON (Нормальный js обьект)
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
-          onLoad(xhr.response); // ответ
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения');
-      });
-
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-      });
+      var xhr = xhrSetup(onLoad, onError);
 
       xhr.open('POST', URL);
       xhr.send(data);
@@ -28,24 +34,7 @@
 
     load: function (onLoad, onError) {
       var URL = 'https://js.dump.academy/code-and-magick/data';
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
-          onLoad(xhr.response);
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения');
-      });
-
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-      });
+      var xhr = xhrSetup(onLoad, onError);
 
       xhr.open('GET', URL);
       xhr.send();
